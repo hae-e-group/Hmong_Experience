88b6cf634aed39e951fb19b3844b3113bee55e64
Eunseok <egg6909@gmail.com>
2019-08-02 02:15:09 +0900

    HES: 수빈양 부탁혀~

--- src/blog.php
+++ src/blog.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.carousel.css">
     <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.theme.default.css">
     <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/animate.css">
+    <link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
     <link href="plugins/jquery-datepicker/jquery-ui.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="styles/common.css">
     <link rel="stylesheet" type="text/css" href="styles/blog.css">
@@ -62,27 +63,42 @@
 
     <!-- Blog -->
 
+
+
     <div class="blog">
         <div class="container">
             <div class="row">
-
                 <!-- Blog Posts -->
                 <div class="col-lg-9">
                     <div class="blog_posts">
-
                         <!-- Blog Post -->
                         <div class="blog_post">
-                            <div class="blog_post_image">
-                                <img src="images/blog_1.jpg" alt="">
-                                <div class="blog_post_date"><a href="#">Oct 20, 2018</a></div>
+                            <div class="row">
+                                <div class="blog_post_image col-6">
+                                    <img src="images/magnet_300x300.jpg" alt="">
+                                    <!--div class="blog_post_date"><a href="#">Oct 20, 2018</a></div-->
+                                </div>
+                                <div class="details_list col-6">
+                                    <ul>
+                                        <li>27 m² Patio</li>
+                                        <li>Balcony with view</li>
+                                        <li>Garden / Mountain view</li>
+                                        <li>Flat-screen TV</li>
+                                        <li>Air conditioning</li>
+                                        <li>Soundproofing</li>
+                                        <li>Private bathroom</li>
+                                        <li>Free WiFi</li>
+                                    </ul>
+                                </div>
                             </div>
+
                             <div class="blog_post_content">
-                                <div class="blog_post_title"><a href="#">Top dream destinations</a></div>
+                                <div class="blog_post_title"><a href="#">Make a Magnet</a></div>
                                 <div class="blog_post_info">
                                     <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
                                         <li class="d-flex flex-row align-items-center justify-content-start">
                                             <img src="images/icon_4.png" alt="">
-                                            <a href="#">News</a>
+                                            <a class="youtube" href="https://www.youtube.com/embed/v64KOxKVLVg">Video</a>
                                         </li>
                                         <li class="d-flex flex-row align-items-center justify-content-start">
                                             <img src="images/icon_5.png" alt="">
@@ -107,6 +123,7 @@
                                         sit amet neque sed elementum. Fusce ut turpis felis. Etiam pretium pharetra
                                         augue, ac porttitor dolor consequat eget. Cras tempor suscipit enim vehicula
                                         ultrices. Mauris sed orci blandit.</p>
+
                                 </div>
                                 <div class="button blog_post_button"><a href="#">Read More</a></div>
                             </div>
@@ -211,24 +228,14 @@
                 <!-- Sidebar -->
                 <div class="col-lg-3">
                     <div class="sidebar">
-
-                        <!-- Search -->
-                        <div class="sidebar_search">
-                            <form action="#" class="sidebar_search_form" id="sidebar_saerch_form">
-                                <input type="text" class="sidebar_search_input" placeholder="Keyword"
-                                       required="required">
-                                <button class="sidebar_search_button">Search</button>
-                            </form>
-                        </div>
-
                         <!-- Recent Posts -->
                         <div class="recent_posts">
-                            <div class="sidebar_title"><h4>Recent Posts</h4></div>
+                            <div class="sidebar_title"><h4>One Day Class</h4></div>
                             <div class="sidebar_list">
                                 <ul>
-                                    <li><a href="#">Featured Product</a></li>
-                                    <li><a href="#">Standard Post</a></li>
-                                    <li><a href="#">Gallery Post</a></li>
+                                    <li><a href="#">Make a Magnet</a></li>
+                                    <li><a href="#">Make a Bracelet</a></li>
+                                    <li><a href="#">Make a Postcard</a></li>
                                     <li><a href="#">Video Post</a></li>
                                     <li><a href="#">Audio Post</a></li>
                                 </ul>
@@ -237,45 +244,15 @@
 
                         <!-- Categories -->
                         <div class="categories">
-                            <div class="sidebar_title"><h4>Categories</h4></div>
+                            <div class="sidebar_title"><h4>Master Class</h4></div>
                             <div class="sidebar_list">
                                 <ul>
-                                    <li><a href="#">News</a></li>
-                                    <li><a href="#">Hotel</a></li>
-                                    <li><a href="#">Vacation</a></li>
+                                    <li><a href="#">Make a Magnet</a></li>
+                                    <li><a href="#">Make a Bracelet</a></li>
+                                    <li><a href="#">Make a Postcard</a></li>
                                 </ul>
                             </div>
                         </div>
-
-                        <!-- Tags -->
-                        <div class="tags">
-                            <div class="sidebar_title"><h4>Tags</h4></div>
-                            <div class="tags_container">
-                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
-                                    <li><a href="#">news</a></li>
-                                    <li><a href="#">hotel</a></li>
-                                    <li><a href="#">vacation</a></li>
-                                    <li><a href="#">reservation</a></li>
-                                    <li><a href="#">booking</a></li>
-                                    <li><a href="#">video</a></li>
-                                    <li><a href="#">clients</a></li>
-                                    <li><a href="#">reviews</a></li>
-                                    <li><a href="#">destinations</a></li>
-                                    <li><a href="#">swimming pool</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <!-- Special Offer -->
-                        <div class="special_offer">
-                            <div class="background_image" style="background-image:url(images/special_offer.jpg)"></div>
-                            <div class="special_offer_container text-center">
-                                <div class="special_offer_title">Special Offer</div>
-                                <div class="special_offer_subtitle">Family Room</div>
-                                <div class="button special_offer_button"><a href="#">Book now</a></div>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
 
@@ -299,6 +276,16 @@
 <script src="plugins/progressbar/progressbar.min.js"></script>
 <script src="plugins/parallax-js-master/parallax.min.js"></script>
 <script src="plugins/jquery-datepicker/jquery-ui.js"></script>
+<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
 <script src="js/blog.js"></script>
+
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+    })
+
+</script>
+
 </body>
 </html>
