6be1733f99722434eb0eabc16e45e964ed15fd55
Eunseok <egg6909@gmail.com>
2019-08-04 01:10:10 +0900

    HES: edit html to php at people

--- src/include/header.php
+++ src/include/header.php
@@ -6,7 +6,7 @@
             <nav class="main_nav" id="main_nav_menu">
                 <ul class="d-flex flex-row align-items-start justify-content-start">
                     <li class="active"><a href="index.php">Home</a></li>
-                    <li><a href="about_hmong.php">About Hmong</a></li>
+                    <li><a href="people.php">About Hmong</a></li>
                     <li><a href="pattern.php">About Patterns</a></li>
                     <li><a href="blog.php">Classes</a></li>
                     <li><a href="contact.php">Contact</a></li>
@@ -32,7 +32,7 @@
         <nav class="menu_nav text-right">
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="about_hmong.php">About Hmong</a></li>
+                <li><a href="people.php">About Hmong</a></li>
                 <li><a href="pattern.php">About Patterns</a></li>
                 <li><a href="blog.php">Blog</a></li>
                 <li><a href="contact.php">Contact</a></li>
---
+++ src/people.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>People</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="The River template project">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+    <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.theme.default.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/animate.css">
+    <link href="plugins/jquery-datepicker/jquery-ui.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="styles/common.css">
+    <link rel="stylesheet" type="text/css" href="styles/people.css">
+    <link rel="stylesheet" type="text/css" href="styles/people_responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+
+    <?php include('include/header.php') ?>
+
+    <!-- Home -->
+
+    <div class="home">
+        <div class="background_image" style="background-image:url(images/hmong_background.jpg)"></div>
+        <div class="home_container">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="home_content text-center">
+                            <div class="home_title">Hmong Craft Women</div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Testimonials -->
+
+    <div class="testimonials">
+        <div class="container">
+            <div class="testimonial_whole_title">Hmong Craft Women</div>
+            <div class="row">
+                <div class="col">
+                    <div class="testimonials_slider_container">
+
+                        <!-- Testimonials Slider -->
+                        <div class="owl-carousel owl-theme test_slider">
+
+                            <!-- Slide -->
+                            <div class="test_slider_item text-center">
+                                <div class="testimonial_title"><a href="javascript:show_layer('Yeonghan');">Yeonghan
+                                        Kwon</a></div>
+                                <div class="testimonial_image"><a href="javascript:show_layer('Yeonghan');"><img
+                                                src="images/yeong_han.jpg" alt=""></a></div>
+                                <div class="testimonial_text">
+                                    <p>He is EVP member. he likes playing and hanging out with his friends.</p>
+                                </div>
+                                <div class="testimonial_sns_img"><img src="images/instagram_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="https://www.instagram.com/honey.han_e/">Instagram</a>
+                                </div>
+                                <div class="testimonial_sns_img"><img src="images/facebook_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Facebook</a></div>
+                            </div>
+                            <!-- Slide -->
+                            <div class="test_slider_item text-center">
+                                <div class="testimonial_title"><a href="javascript:show_layer('Soobin');">Soobin Kim</a>
+                                </div>
+                                <div class="testimonial_image"><a href="javascript:show_layer('Soobin');"><img
+                                                src="images/soo_bin.jpg" alt=""></a></div>
+                                <div class="testimonial_text">
+                                    <p>She had long hair but now, she has short hair.</p>
+                                </div>
+                                <div class="testimonial_sns_img"><img src="images/instagram_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Instagram</a></div>
+                                <div class="testimonial_sns_img"><img src="images/facebook_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Facebook</a></div>
+                            </div>
+                            <!-- Slide -->
+                            <div class="test_slider_item text-center">
+                                <div class="testimonial_title"><a href="javascript:show_layer('Chulwoo');">Chulwoo
+                                        Kim</a></div>
+                                <div class="testimonial_image"><a href="javascript:show_layer('Chulwoo');"><img
+                                                src="images/chul_woo.jpg" alt=""></a></div>
+                                <div class="testimonial_text">
+                                    <p>He lives in InGyeDong the hottest place in Suwon.</p>
+                                </div>
+                                <div class="testimonial_sns_img"><img src="images/instagram_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Instagram</a></div>
+                                <div class="testimonial_sns_img"><img src="images/facebook_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Facebook</a></div>
+                            </div>
+                            <!-- Slide -->
+                            <div class="test_slider_item text-center">
+                                <div class="testimonial_title"><a href="javascript:show_layer('Eunsok');">Eunsok
+                                        Hong</a></div>
+                                <div class="testimonial_image"><a href="javascript:show_layer('Eunsok');"><img
+                                                src="images/eun_seok.jpg" alt=""></a></div>
+                                <div class="testimonial_text">
+                                    <p>He is the leader of E group. He cannot eat spicy food at all.</p>
+                                </div>
+                                <div class="testimonial_sns_img"><img src="images/instagram_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Instagram</a></div>
+                                <div class="testimonial_sns_img"><img src="images/facebook_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Facebook</a></div>
+                            </div>
+                            <!-- Slide -->
+                            <div class="test_slider_item text-center">
+                                <div class="testimonial_title"><a href="javascript:show_layer('Chulmin');">Chulmin
+                                        Yeom</a></div>
+                                <div class="testimonial_image"><a href="javascript:show_layer('Chulmin');"><img
+                                                src="images/chul_min.jpg" alt=""></a></div>
+                                <div class="testimonial_text">
+                                    <p>He is the artist of Hae. He has the mission that create high quality video.</p>
+                                </div>
+                                <div class="testimonial_sns_img"><img src="images/instagram_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Instagram</a></div>
+                                <div class="testimonial_sns_img"><img src="images/facebook_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Facebook</a></div>
+                            </div>
+                            <!-- Slide -->
+                            <div class="test_slider_item text-center">
+                                <div class="testimonial_title"><a href="javascript:show_layer('Eunsol');">Eunsol
+                                        Choi</a></div>
+                                <div class="testimonial_image"><a href="javascript:show_layer('Eunsol');"><img
+                                                src="images/eun_sol.jpg" alt=""></a></div>
+                                <div class="testimonial_text">
+                                    <p>She is in Education team. she is so ardent that she made studio in her house.</p>
+                                </div>
+                                <div class="testimonial_sns_img"><img src="images/instagram_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Instagram</a></div>
+                                <div class="testimonial_sns_img"><img src="images/facebook_icon.jpg"></div>
+                                <div class="testimonial_sns d-flex"><a href="#">Facebook</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        function show_layer(div_name) {
+            document.all.yeonghan.style.display = "none";
+            document.all.soobin.style.display = "none";
+            document.all.chulwoo.style.display = "none";
+            document.all.eunseok.style.display = "none";
+            document.all.chulmin.style.display = "none";
+            document.all.eunsol.style.display = "none";
+            switch (div_name) {
+                case 'Yeonghan':
+                    document.all.yeonghan.style.display = "";
+                    break;
+                case 'Soobin':
+                    document.all.soobin.style.display = "";
+                    break;
+                case 'Chulwoo':
+                    document.all.chulwoo.style.display = "";
+                    break;
+                case 'Eunsok':
+                    document.all.eunseok.style.display = "";
+                    break;
+                case 'Chulmin':
+                    document.all.chulmin.style.display = "";
+                    break;
+                case 'Eunsol':
+                    document.all.eunsol.style.display = "";
+                    break;
+            }
+        }
+    </script>
+
+    <div id=yeonghan style="DISPLAY: none">
+        <div class="container">
+            <div class="about_women">
+                <div class="row d-flex">
+                    <div class="col-lg-6">
+                        <img class="about_women_img" src="images/yeong_han.jpg">
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="about_women_title">Yeonghan Kwon</div>
+                        <p>Yeonghan is ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~
+                            ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~ ~~~~.</p>
+                        <div class="about_women_subtitle">Techniques</div>
+                        <li>Acapella (9 years)</li>
+                        <li>Taekwondo (4 Dan)</li>
+                        <li>Drum (3 months)</li>
+                        <div class="about_women_subtitle">Classes</div>
+                        <li><a href="#">Fundamental Acapella</a></li>
+                        <li><a href="#">Advanced Taekwondo</a></li>
+                    </div>
+                </div>
+                <div id="instaPics"></div>
+            </div>
+        </div>
+    </div>
+    <div id=soobin style="DISPLAY: none">
+        <div class="about_women">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="about_women_title">Soobin Kim</div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="images/soo_bin.jpg">
+                </div>
+                <div class="row about_row">
+                    <div class="col-lg-6">
+                        <div class="about_content">
+                            <div class="about_text">
+                                <p>My name is Soobin. My name is Soobin. My name is Soobin. My name is Soobin. My name
+                                    is Soobin. My name is Soobin. My name is Soobin. My name is Soobin. My name is
+                                    Soobin. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id=chulwoo style="DISPLAY: none">
+        <div class="about_women">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="about_women_title">Chulwoo Kim</div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="images/chul_woo.jpg">
+                </div>
+                <div class="row about_row">
+                    <div class="col-lg-6">
+                        <div class="about_content">
+                            <div class="about_text">
+                                <p>My name is Chulwoo. My name is Chulwoo. My name is Chulwoo. My name is Chulwoo. My
+                                    name is Chulwoo. My name is Chulwoo. My name is Chulwoo. My name is Chulwoo. My name
+                                    is Chulwoo. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id=eunseok style="DISPLAY: none">
+        <div class="about_women">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="about_women_title">Eunseok Hong</div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="images/eun_seok.jpg">
+                </div>
+                <div class="row about_row">
+                    <div class="col-lg-6">
+                        <div class="about_content">
+                            <div class="about_text">
+                                <p>My name is Eunseok. My name is Eunseok. My name is Eunseok. My name is Eunseok. My
+                                    name is Eunseok. My name is Eunseok. My name is Eunseok. My name is Eunseok. My name
+                                    is Eunseok. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id=chulmin style="DISPLAY: none">
+        <div class="about_women">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="about_women_title">Chulmin Yeom</div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="images/chul_min.jpg">
+                </div>
+                <div class="row about_row">
+                    <div class="col-lg-6">
+                        <div class="about_content">
+                            <div class="about_text">
+                                <p>My name is Chulmin. My name is Chulmin. My name is Chulmin. My name is Chulmin. My
+                                    name is Chulmin. My name is Chulmin. My name is Chulmin. My name is Chulmin. My name
+                                    is Chulmin. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id=eunsol style="DISPLAY: none">
+        <div class="about_women">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="about_women_title">Eunsol Choi</div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="images/eun_sol.jpg">
+                </div>
+                <div class="row about_row">
+                    <div class="col-lg-6">
+                        <div class="about_content">
+                            <div class="about_text">
+                                <p>My name is Eunsol. My name is Eunsol. My name is Eunsol. My name is Eunsol. My name
+                                    is Eunsol. My name is Eunsol. My name is Eunsol. My name is Eunsol. My name is
+                                    Eunsol. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include('include/footer.php') ?>
+</div>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="styles/bootstrap-4.1.2/popper.js"></script>
+<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/OwlCarousel2-2.3.4/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/progressbar/progressbar.min.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="plugins/jquery-datepicker/jquery-ui.js"></script>
+<script src="js/people.js"></script>
+</body>
+</html>
